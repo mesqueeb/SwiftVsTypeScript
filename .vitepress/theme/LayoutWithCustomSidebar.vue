a7ae9d109cc945666b25fd58aa4ecb8b45165810
mesqueeb <mesqueeb@users.noreply.github.com>
2024-07-29 09:08:24 +0900

    fix: small CSS tweak

--- .vitepress/theme/LayoutWithCustomSidebar.vue
+++ .vitepress/theme/LayoutWithCustomSidebar.vue
@@ -8,12 +8,16 @@ const { Layout } = DefaultTheme
 <template>
   <Layout>
     <template #sidebar-nav-after>
-      <VPDocAside />
+      <VPDocAside class="aside-in-sidebar" />
     </template>
   </Layout>
 </template>
 
 <style>
+.aside-in-sidebar {
+  z-index: 0;
+  position: relative;
+}
 div.aside {
 	display: none !important;
 }
